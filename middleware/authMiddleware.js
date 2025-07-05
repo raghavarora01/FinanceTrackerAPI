2910e4157430e680cebc4f00efaf371399f87d35
Raghav Arora <97450013+raghavarora01@users.noreply.github.com>
2025-07-05 17:51:02 +0530

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,31 +1,3 @@
-// import jwt from 'jsonwebtoken';
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// const JWT_SECRET = process.env.JWT_SECRET;
-
-// const protect = (req, res, next) => {
-//   const authHeader = req.headers.authorization;
-
-//   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//     return res.status(401).json({ message: 'No token, authorization denied' });
-//   }
-
-//   const token = authHeader.split(' ')[1];
-
-//   try {
-//     const decoded = jwt.verify(token, JWT_SECRET);
-//     req.user = decoded; // You can also do: req.userId = decoded.id;
-//     next();
-//   } catch (err) {
-//     return res.status(401).json({ message: 'Token is not valid' });
-//   }
-// };
-
-// export default protect;
-
-
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
